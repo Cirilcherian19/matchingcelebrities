0629e97bc332686d40036f8c20919a034a56204c
CIRIL CHERIAN <cirilcherian009@gmail.com>
2023-11-28 16:38:17 +0530

    data ingestion completed

---
+++ src/utils/all_utils.py
@@ -0,0 +1,17 @@
+import yaml
+import os
+import logging
+from bing_image_downloader import downloader
+
+
+def read_yaml(path_to_yaml: str) -> dict:
+    with open(path_to_yaml) as yaml_file:
+        content = yaml.safe_load(yaml_file)
+
+    return content
+
+
+def create_directory(dirs: list):
+    for dir_path in dirs:
+        os.makedirs(dir_path, exist_ok=True)
+        logging.info(f"Directory is created at {dir_path}")
